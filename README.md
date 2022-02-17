ee36f4c6f7a6a0fb518cf0b912455f5f46ad1d44
Rahulsrinivasan30 <rahulsrinivasan30@gmail.com>
2022-02-17 19:27:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-y4vpns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-y4vpns)
---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('hello!');
+
+/**
+ * Recreate the given figma design.
+ * Figma - https://www.figma.com/file/WqOQpn3UALl5sRKzr4TyZF/Conc-Web-Dev-Task?node-id=27%3A107
+ *
+ * For image - https://picsum.photos/200
+ * For Assets - https://drive.google.com/file/d/1jy0mNAvywpNMv9YW7sEyONtfqjoqGodD/view
+ */
